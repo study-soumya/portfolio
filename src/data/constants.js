5b2484247954e33058297195a5279d2a4ec9912a
Soumya Ranjan Patra <sonusoum.study@gmail.com>
2024-09-23 14:24:11 +0530

    updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -98,11 +98,6 @@ export const skills = [
 		title: "Others",
 		skills: [
 			{
-				name: "Git",
-				image:
-					"https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
-			},
-			{
 				name: "GitHub",
 				image:
 					"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
